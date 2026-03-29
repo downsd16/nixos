fb1388bbed0ef252ceaba786e8631e55d24bca4a
downsd16 <downsd16@gmail.com>
2026-03-29 18:55:29 -0400

    make and add hardware.nix to manager default

--- hosts/manager/default.nix
+++ hosts/manager/default.nix
@@ -22,18 +22,4 @@
     openFirewall = true;
     # advertiseAddr = "10.0.0.10";
   };
-
-  # Nice-to-have for remote access
-  # services.avahi.enable = true;
-  # services.avahi.nssmdns4 = true;
-
-  # If using the Swarm-based Guacamole module we built earlier:
-  #services.guacamoleSwarm = {
-  #  enable = true;
-  #  webPort = 8080;
-  #  managerConstraint = "node.labels.role == manager";
-  #  workerConstraint  = "node.labels.role == worker";
-  #  guacdReplicas = 1;
-  #  dbPasswordFile = "/etc/guacamole/db-password";
-  };
 }
